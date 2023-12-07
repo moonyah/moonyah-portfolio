b97a3cf59d741c8012456ee60d17854940683576
moonyah <dltk456@naver.com>
2023-12-08 00:41:45 +0900

    Feat: initial setting and header layout

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutMe: React.FC = () => {
+  return (
+    <section id="about-me">
+      <h2>About Me</h2>
+      {/* About Me 섹션의 내용을 추가하세요 */}
+      <p>This is the About Me section.</p>
+    </section>
+  );
+};
+
+export default AboutMe;
---
+++ src/components/Career.tsx
@@ -0,0 +1,15 @@
+// components/Career.tsx
+
+import React from 'react';
+
+const Career: React.FC = () => {
+  return (
+    <section id="career">
+      <h2>Career</h2>
+      {/* Career 섹션의 내용을 추가하세요 */}
+      <p>This is my career history.</p>
+    </section>
+  );
+};
+
+export default Career;

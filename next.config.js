1692e3fc97ed3b12e24ef673f7b44853f0d49894
moonyah <dltk456@naver.com>
2023-12-08 04:53:03 +0900

    Feat: deploy setting for github pages

--- next.config.js
+++ next.config.js
@@ -1,17 +1,3 @@
-// next.config.js
-
-module.exports = {
-  // 다른 설정...
-
-  // 정적 내보내기를 위한 빌드
-  target: 'serverless',
-
-  // 정적 내보내기를 위한 경로 매핑
-  exportPathMap: async function () {
-    return {
-      '/': { page: '/' },
-      '/_not-found': { page: '/_not-found' },
-      // 필요한 경우 다른 페이지 추가
-    };
-  },
+const nextConfig = {
+  basePath: '/',
 };

b34aa99c42d972790a2a56f8727cf0f8f9e1e2d0
moonyah <dltk456@naver.com>
2023-12-08 11:43:09 +0900

    Feat: write in components

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,12 +4,12 @@ const Skills: React.FC = () => {
   return (
     <section id="skills" className="py-8">
       <div className="container mx-auto">
-        <h2 className="text-4xl font-bold mb-4 text-yellow-50">Skills</h2>
+        <h2 className="text-4xl font-bold mb-4 text-yellow-300">Skills</h2>
 
         {/* 기술 목록 */}
         <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           <li className="border p-4 rounded-md border-gray-700">
-            <h3 className="text-xl font-semibold mb-2">
+            <h3 className="text-xl font-semibold mb-2 text-yellow-100">
               Programming Languages
             </h3>
             <ul className="list-disc pl-6 text-gray-400">
@@ -20,7 +20,9 @@ const Skills: React.FC = () => {
           </li>
 
           <li className="border p-4 rounded-md border-gray-700">
-            <h3 className="text-xl font-semibold mb-2">Frontend</h3>
+            <h3 className="text-xl font-semibold mb-2 text-yellow-100">
+              Frontend
+            </h3>
             <ul className="list-disc pl-6 text-gray-400">
               <li>HTML5</li>
               <li>CSS3</li>
@@ -36,7 +38,9 @@ const Skills: React.FC = () => {
           </li>
 
           <li className="border p-4 rounded-md border-gray-700">
-            <h3 className="text-xl font-semibold mb-2">Tools</h3>
+            <h3 className="text-xl font-semibold mb-2 text-yellow-100">
+              Tools
+            </h3>
             <ul className="list-disc pl-6 text-gray-400">
               <li>Trello</li>
               <li>Jira</li>
